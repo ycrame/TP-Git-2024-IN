1a2ca688b858663778f60b211a1196e0928b95f9
Shaunerie <luc.samain@gmail.com>
2023-11-30 15:34:08 +0100

    fact

--- samain-sanchez/racine.py
+++ samain-sanchez/racine.py
@@ -1,16 +1,13 @@
 import sys
 
-def fibonnaci(n):
+def fact(n):
     if n == 0:
-        return 0
-    elif n == 1:
         return 1
     else:
-        fib = fibonnaci(n-1) + fibonnaci(n-2)
-        return fib
+        return n * fact(n-1)
 
 def main(argv):
-    print(fibonnaci(int(argv[1])))
+    print(fact(int(argv[1])))
 
 if __name__ == "__main__":
     main(sys.argv)
