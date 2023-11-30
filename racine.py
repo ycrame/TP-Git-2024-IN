6be11ef180c3d545310a312726d6ef401490fdbb
Shaunerie <luc.samain@gmail.com>
2023-11-30 15:07:53 +0100

    premier code la meilleur team

---
+++ racine.py
@@ -0,0 +1,15 @@
+def fibonnaci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fib = fibonnaci(n-1) + fibonnaci(n-2)
+        return fib
+
+def main(argv):
+    print(fibonnaci(int(argv[1])))
+
+if __name__ == "__main__":
+    main(sys.argv)
+    
